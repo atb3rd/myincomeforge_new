1eab87003c1abcbbb76d65b0aad310c0a32396b7
atb3rd <albaker777@gmail.com>
2026-02-18 12:31:21 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: "40px", fontFamily: "Arial"}}>
+      <h1>My Income Forge</h1>
+      <p>Your US income calculator hub is coming soon.</p>
+    </main>
+  );
+}
